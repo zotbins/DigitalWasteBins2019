acf3284eca803898a51f8f444fd62d0f000cb70a
okyang <owenyang123@gmail.com>
2019-03-28 14:35:15 -0700

    working animation with updated images

--- dwb_app.py
+++ dwb_app.py
@@ -12,18 +12,37 @@ https://www.tutorialspoint.com/pyqt/
 
 SVG Display:
 https://stackoverflow.com/questions/35129102/simple-way-to-display-svg-image-in-a-pyqt-window
+
+Animations:
+http://zetcode.com/pyqt/qpropertyanimation/
+https://www.programcreek.com/python/example/99577/PyQt5.QtCore.QPropertyAnimation
 """
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
-from PyQt5 import QtCore, QtSvg
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QFrame, QPushButton
+from PyQt5 import QtCore
+from PyQt5.QtCore import QPropertyAnimation, QPointF, pyqtProperty, Qt
 from PyQt5.QtGui import QPixmap
 
 import time
 
+class WasteImage(QLabel):
+    def __init__(self,parent,image_file):
+        super().__init__(parent)
+        pix = QPixmap(image_file)
+        pix = pix.scaled(2000.000 / 10, 6000.000 / 10, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+        self.h = 2000.000/10
+        self.w = 6000.000/10
+        self.setPixmap(pix)
+
+    def _set_pos(self, pos):
+        self.move(pos.x(), pos.y())
+
+    pos = pyqtProperty(QPointF, fset=_set_pos)
+
 class App(QWidget):
 
     def __init__(self):
-        super().__init__() #inhreitance from QWidget
+        super().__init__()#inhreitance from QWidget
         self.title = 'PyQT Window'
 
         #determines where the window will be created
@@ -31,9 +50,14 @@ class App(QWidget):
         self.top = 50
 
         #determines the size of the window
-
         self.width = 5038.176/10
         self.height = 9135.347/10
+
+        #determines background color of the window
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(p)
 
         #initialized the window
         self.initUI()
@@ -43,22 +67,48 @@ class App(QWidget):
         self.setGeometry(self.left, self.top, self.width, self.height)
         # self.statusBar().showMessage('Message in statusbar.')
 
+        # label = QLabel(self)
+        # label.move(1508.264/10, 2132.126/10)
+        # pixmap = QPixmap('images/compost/c1.png')
+        # pixmap = pixmap.scaled(2000.000/10, 6000.000/10, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+        # label.setPixmap(pixmap)
+        # # Uncomment to resize window size to be the same size as the image
+        # # self.resize(pixmap.width(), pixmap.height())
+
+        self.WasteImage = WasteImage(self, 'images/compost/c2.png')
+        self.initAnim()
+
+        #this is the image that is supposed to serve as the frame
         background = QLabel(self)
         back_pixmap = QPixmap('images/compost/compost_background.png') #image.jpg (5038,9135)
         back_pixmap = back_pixmap.scaled(5038/10, 9135/10, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
         background.setPixmap(back_pixmap)
 
-        label = QLabel(self)
-        label.move(1508.264/10, 2132.126/10)
-        pixmap = QPixmap('images/compost/c1.png')
-        pixmap = pixmap.scaled(2000.000/10, 6000.000/10, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
-        label.setPixmap(pixmap)
-
-        # Uncomment to resize window size to be the same size as the image
-        # self.resize(pixmap.width(), pixmap.height())
-
         #self.showFullScreen()
         self.show() #uncomment if you don't want fullscreen
+
+
+        # self.button = QPushButton("Start", self)
+        # self.button.clicked.connect(self.doAnim)
+        # self.button.move(30, 30)
+        #
+        # self.frame = QFrame(self)
+        # self.frame.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        # self.frame.setGeometry(150, 30, 100, 100)
+        #
+        # self.setGeometry(300, 300, 380, 300)
+        # self.setWindowTitle('Animation')
+
+        self.show()
+
+    def initAnim(self):
+        self.anim = QPropertyAnimation(self.WasteImage,b"pos")
+        self.anim.setDuration(3000)
+
+        self.anim.setStartValue(QPointF(10, 2132.126/10))
+        self.anim.setEndValue(QPointF(1508.264/10, 2132.126/10))
+
+        self.anim.start()
 
 if __name__ == "__main__":
     #creating new class
