97dff0839090c6801e9e27bfe8ffafaa0add8d00
okyang <owenyang123@gmail.com>
2019-04-19 20:25:34 -0700

    starting to integrate breakbeam code with pyqt

---
+++ dwb_raspi_app.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+last_state=0
+
+while True:
+    sensor_state = GPIO.input(4)
+    #sensor_state = sum([GPIO.input(4) for i in range(10)] )
+    if (sensor_state==0):
+        while(sensor_state==0):
+            pass
+        print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+    """
+    if(sensor_state==1 and last_state==0 ):
+        print("Unbroken")
+    if(sensor_state==0 and last_state==1):
+        print("broken")
+    last_state = sensor_state
+    """
