50aaf6c50489e665b3ca78ccd4db6cedbbb38a8e
okyang <owenyang123@gmail.com>
2019-04-21 11:43:22 -0700

    adding stuff

--- dependency_tests/breakbeam_test.py
+++ dependency_tests/breakbeam_test.py
@@ -3,24 +3,18 @@ import datetime
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
+GPIO.setup(17,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
 
-last_state=0
+
 
 while True:
     sensor_state = GPIO.input(4)*GPIO.input(17)*GPIO.input(27)
-    #sensor_state = sum([GPIO.input(4) for i in range(10)] )
+    print([GPIO.input(4),GPIO.input(17), GPIO.input(27)])
     if (sensor_state==0):
         while(sensor_state==0):
             sensor_state = GPIO.input(4)*GPIO.input(17)*GPIO.input(27)
-        self.my_signal.emit()
-        time.sleep(2)
-
-        print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-    """
-    if(sensor_state==1 and last_state==0 ):
-        print("Unbroken")
-    if(sensor_state==0 and last_state==1):
-        print("broken")
-    last_state = sensor_state
-    """
-    time.sleep(.02)
+            print([GPIO.input(4),GPIO.input(17), GPIO.input(27)])
+            print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+            time.sleep(1)
+    time.sleep(1)
