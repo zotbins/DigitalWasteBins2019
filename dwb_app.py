cdcd1ac830dfc20985f9d49618647bc51ec22d7c
TranATT <atran750@gmail.com>
2019-11-15 17:54:34 -0800

    Martin Gomez

--- dwb_app.py
+++ dwb_app.py
@@ -1,5 +1,5 @@
 """
-Authors: Owen Yang, Tristan Samonte, Anthony Esmeralda, TingTing Tsai, Alpine Tang, Andy Tran
+Authors: Owen Yang, Tristan Samonte, Anthony Esmeralda, TingTing Tsai, Alpine Tang, Andy Tran, Martin Gomez
 Dependencies:
 sudo apt-get install qt5-default
 sudo apt-get install qt5-default pyqt5-dev pyqt5-dev-tools
